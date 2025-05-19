e4857630fc4cfe52524cb0e3e0981f989552c8e4
yang7129 <yang7129@gmail.com>
2025-05-19 13:58:26 +0800

    JWT_exmple_HS256

---
+++ Console-JWT-Example/Base.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_JWT_Example
+{
+    internal class Base
+    {
+        // Base64Url 編碼（去掉 =、替換 URL 安全字元）
+        protected string Base64UrlEncode(byte[] input)
+        {
+            return Convert.ToBase64String(input).Replace('+', '-').Replace('/', '_').Replace("=", "");
+        }
+        // Base64Url 解碼（補上 =、還原 URL 安全字元）
+        protected byte[] Base64UrlDecode(string input)
+        {
+            string padded = input.Replace('-', '+').Replace('_', '/');
+            switch (padded.Length % 4)
+            {
+                case 2: padded += "=="; break;
+                case 3: padded += "="; break;
+                case 1: padded += "==="; break;
+            }
+            return Convert.FromBase64String(padded);
+        }
+        protected string Base64UrlDecodeToString(string input)
+        {
+            string padded = input.Replace('-', '+').Replace('_', '/');
+            switch (padded.Length % 4)
+            {
+                case 2: padded += "=="; break;
+                case 3: padded += "="; break;
+                case 1: padded += "==="; break;
+            }
+            return Encoding.UTF8.GetString(Convert.FromBase64String(padded));
+        }
+    }
+}
---
+++ Console-JWT-Example/JWT/JWT.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Macs;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Digests;
+using System.Text.Json;
+
+namespace Console_JWT_Example.JWT
+{
+    internal class JWT: Base
+    {
+        public void exmple_HS256()
+        {
+            Console.WriteLine("exmple_HS256");
+            // JWT Token
+            // JWT Value = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ind3dy5iZWpzb24uY29tIiwic3ViIjoiZGVtbyIsImlhdCI6MTc0NzYxODA0MywibmJmIjoxNzQ3NjE4MDQzLCJleHAiOjE3NDc3MDQ0NDN9.b_8Zch2tJe2lgEOC0ctRjPOCvyyaG3siLxW2-iYGuoE
+            // Header = {"alg": "HS256","typ": "JWT"}
+            // Payload ={"username": "www.bejson.com","sub": "demo","iat": 1747618043,"nbf": 1747618043,"exp": 1747704443}
+            // HMAC Key = bejson 
+
+            Console.WriteLine("符合 RFC 7519 的 JWT："); 
+            
+            string headerJson = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+            //  encodedHeader = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+            string encodedHeader = Base64UrlEncode(Encoding.UTF8.GetBytes(headerJson));
+            // Decode JWT Headjson
+            Console.WriteLine(Base64UrlDecodeToString("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"));
+           
+
+            string payloadJson = "{\"username\":\"www.bejson.com\",\"sub\":\"demo\",\"iat\":1747618043,\"nbf\":1747618043,\"exp\":1747704443}";
+            // encodedPayload = eyJ1c2VybmFtZSI6Ind3dy5iZWpzb24uY29tIiwic3ViIjoiZGVtbyIsImlhdCI6MTc0NzYxODA0MywibmJmIjoxNzQ3NjE4MDQzLCJleHAiOjE3NDc3MDQ0NDN9
+            string encodedPayload = Base64UrlEncode(Encoding.UTF8.GetBytes(payloadJson));
+            // Decode JWT payloadJson
+            Console.WriteLine(Base64UrlDecodeToString("eyJ1c2VybmFtZSI6Ind3dy5iZWpzb24uY29tIiwic3ViIjoiZGVtbyIsImlhdCI6MTc0NzYxODA0MywibmJmIjoxNzQ3NjE4MDQzLCJleHAiOjE3NDc3MDQ0NDN9"));
+
+
+            // encodedHeader.encodedPayload
+            string signingInput = $"{encodedHeader}.{encodedPayload}";
+             
+            string key = "bejson";
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+            byte[] signatureBytes;
+            using (var hmac = new HMACSHA256(keyBytes))
+            {
+                signatureBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(signingInput));
+            }
+            string encodedSignature = Base64UrlEncode(signatureBytes);
+            string jwt = $"{signingInput}.{encodedSignature}";
+            Console.WriteLine("JWT:");
+            Console.WriteLine(jwt);
+            string expected = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ind3dy5iZWpzb24uY29tIiwic3ViIjoiZGVtbyIsImlhdCI6MTc0NzYxODA0MywibmJmIjoxNzQ3NjE4MDQzLCJleHAiOjE3NDc3MDQ0NDN9.b_8Zch2tJe2lgEOC0ctRjPOCvyyaG3siLxW2-iYGuoE";
+
+            Console.WriteLine("Check？jwt=expected |" + (jwt == expected)); 
+        }  
+    }
+}
